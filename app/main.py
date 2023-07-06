d54c608ce303e2be3771c4829753d5af09a62079
mappedev <mappedev@gmail.com>
2023-07-06 16:11:58 -0400

    add cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.routes import include_router
+from utils.middlewares import include_middlewares
 
 """ To init DB automatically """
 # from commons.database.db import Base, engine
@@ -10,6 +12,7 @@ from routers.routes import include_router
 
 app = FastAPI(title="Twitter API", version="0.0.1")
 
+include_middlewares(app=app)
 include_router(app=app)
 
 if __name__ == "__main__":
