2ed48794d1a026af074de46b5d85b0d4c916a7a4
mappedev <mappedev@gmail.com>
2023-07-17 23:33:32 -0400

    add cors

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 
 from routers.routes import include_router
 from config.settings import settings
-# from utils.middlewares import include_middlewares
+from utils.middlewares import include_middlewares
 
 """ To init DB automatically """
 # from commons.database.db import Base, engine
@@ -16,7 +16,7 @@ app.get('/')
 def home():
     return {'message': 'Hello World'}
 
-# include_middlewares(app=app)
+include_middlewares(app=app)
 include_router(app=app)
 
 if __name__ == "__main__":
